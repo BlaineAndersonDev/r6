23c76fa16a1ffdc951afba15885d9c714e25292f
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2020-04-05 19:47:40 -0700

    Current selected weather now is removed and replaced when user selected previous option or selects new option

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,9 +1,7 @@
 class WeathersController < ApplicationController
 
   def index
-    puts ">>> Weather Index Route"
-    @raw_weather = Faraday.get 'https://api.openweathermap.org/data/2.5/weather?zip=85205,us&&units=metric&appid=8e6a821f8ce110463faa10800583add5'
-    @weather = JSON(@raw_weather.body)
+    @weathers = Weather.all.order("created_at DESC").limit(10)
   end
 
   def create
